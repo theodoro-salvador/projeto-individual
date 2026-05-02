3f4a649cb0c3012efcbff0a937077ffdbd3b1f1e
theodoro-salvador <theodoro.salvador@sptech.school>
2026-05-02 08:32:10 -0300

    mudanças de organização do script no forum, função de registrar usuário com conexão com banco de dados

---
+++ public/src/js/script.js
@@ -0,0 +1,308 @@
+let form = 0;
+
+    function alterLoginRegis(){
+        
+        if(form == 0){
+
+            form = 1;
+
+            document.getElementById('login_form').style.display = 'none';
+            document.getElementById('register_form').style.display = 'block';
+
+        } else{
+
+            form = 0;
+
+            document.getElementById('register_form').style.display = 'none';
+            document.getElementById('login_form').style.display = 'block';
+
+        }
+
+    }
+
+    let validacaoEmail = 0;
+
+    function validarEmail(){
+
+        let arroba = 0;
+        let ponto = 0;
+
+        document.getElementById('span_arroba').style.display = 'none';
+        document.getElementById('span_ponto').style.display = 'none';
+
+        if(validarArroba()){
+            arroba = 1;
+        } else{
+            arroba = 0;
+
+            document.getElementById('span_arroba').style.display = 'block';
+        }
+
+        if(validarPonto()){
+            ponto = 1;
+        } else{
+            ponto = 0;
+
+            document.getElementById('span_ponto').style.display = 'block';
+        }
+
+        validacaoEmail = arroba + ponto;
+
+        if(validacaoEmail == 2){
+            document.getElementById('validacao_email').style.display = 'none';
+        } else{
+            document.getElementById('validacao_email').style.display = 'flex';
+        }
+
+        
+        // VALIDAÇÃO DE E-MAIL /////////////////////////////////////////////////////////
+        
+        function validarArroba(){
+            
+            let email = input_registrar_email.value;
+            
+            if(email.includes('@')){
+                return true;
+            }
+            
+            return false;
+            
+        }
+
+        function validarPonto(){
+
+            let email = input_registrar_email.value;
+
+            for(let i = 0; i < email.length; i++){
+
+                if(email[i] == '.' && (i > email.indexOf('@') && email.indexOf('@') != -1)){
+                    return true;
+                }
+                
+            }
+            
+            return false;
+            
+        }
+        
+        ///////////////////////////////////////////////////////////////////////////////
+        
+    }
+
+    let validacaoSenha = 0;
+
+    function validarSenha(){
+        
+        let caracteresEspeciais = 0;
+        let tamanho = 0;
+        let caps = 0;
+        let numeros = 0;
+
+
+        document.getElementById('span_caracteres_especiais').style.display = 'none';
+        document.getElementById('span_tamanho').style.display = 'none';
+        document.getElementById('span_caps').style.display = 'none';
+        document.getElementById('span_numeros').style.display = 'none';
+
+
+        if(validarCaracteresEspeciais()){
+            caracteresEspeciais = 1;
+        } else{
+            caracteresEspeciais = 0;
+
+            document.getElementById('span_caracteres_especiais').style.display = 'block';
+            
+            setInterval(function(){
+                document.getElementById('span_caracteres_especiais').style.display = 'none';
+            }, 5000);
+
+        }
+
+        if(validarTamanho()){
+            tamanho = 1;
+
+        } else{
+            tamanho = 0;
+
+            document.getElementById('span_tamanho').style.display = 'block';
+
+            setInterval(function(){
+                document.getElementById('span_tamanho').style.display = 'none';
+            }, 5000);
+        }
+
+        if(validarCaps()){
+            caps = 1;
+
+        } else{
+            caps = 0;
+
+            document.getElementById('span_caps').style.display = 'block';
+
+            setInterval(function(){
+                document.getElementById('span_caps').style.display = 'none';
+            }, 5000);
+        }
+
+        if(validarNumeros()){
+            numeros = 1;
+
+        } else{
+            numeros = 0;
+
+            document.getElementById('span_numeros').style.display = 'block';
+
+            setInterval(function(){
+                document.getElementById('span_numeros').style.display = 'none';
+            }, 5000);
+        }
+
+        validacaoSenha = caracteresEspeciais + tamanho + caps + numeros;
+
+        if(validacaoSenha == 4){
+            document.getElementById('validacao_senha').style.display = 'none';
+        } else{
+            document.getElementById('validacao_senha').style.display = 'flex';
+        }
+        
+        
+        // VALIDAÇÃO DE SENHA //////////////////////////////////////////////////////////////
+        
+        function validarCaracteresEspeciais(){
+            
+            let password = input_registrar_senha.value;
+            let listaCaracteres = ['!','@','#','$','%','&','*','(',')','_','-','+','=',
+            '{','}','[',']','/','|','.',',',';',':','<','>','?'];
+            
+            for(let i = 0; i < listaCaracteres.length; i++){
+                if(password.includes(listaCaracteres[i])){
+                    return true;
+                }
+            }
+            
+            return false;
+            
+        }
+        
+        function validarTamanho(){
+            
+            let password = input_registrar_senha.value;
+            
+            if(password.length >= 8){
+                return true;
+            }
+            
+            return false;
+            
+        }
+        
+        function validarCaps(){
+            
+            let password = input_registrar_senha.value;
+            
+            if(password != password.toLowerCase()
+            && password != password.toUpperCase()){
+        return true;
+    }
+    
+            return false;
+
+        }
+
+        function validarNumeros(){
+
+            let password = input_registrar_senha.value;
+            let listaNumeros = [1,2,3,4,5,6,7,8,9,0];
+            
+            for(let i = 0; i < listaNumeros.length; i++){
+                if(password.includes(listaNumeros[i])){
+                    return true;
+                }
+            }
+
+            return false;
+            
+        }
+        
+        ///////////////////////////////////////////////////////////////////
+        
+    }
+
+    let validacaoConfirmarSenha = 0;
+
+    function validarConfirmacaoSenha(){
+
+        let confirmarSenha = input_confirmar_senha.value;
+        let senha = input_registrar_senha.value;
+console.log('oie')
+        if(confirmarSenha == senha){
+
+            validacaoConfirmarSenha = 1;
+
+            document.getElementById('validacao_confirmar_senha').style.display = 'none';
+            document.getElementById('span_confirmar_senha').style.display = 'none';
+
+        } else{
+            
+            validacaoConfirmarSenha = 0;
+
+            document.getElementById('validacao_confirmar_senha').style.display = 'flex';
+            document.getElementById('span_confirmar_senha').style.display = 'block';
+
+            setInterval(function(){
+                document.getElementById('validacao_confirmar_senha').style.display = 'none';
+                document.getElementById('span_confirmar_senha').style.display = 'none';
+            }, 5000);
+        }
+    }
+
+    function registrar(){
+
+        let usernameLet = input_registrar_username.value;
+        let emailLet = input_registrar_email.value;
+        let senhaLet = input_registrar_senha.value;
+        let confirmarSenha = input_confirmar_senha.value;
+
+        let validacao = validacaoConfirmarSenha + validacaoSenha + validacaoEmail;
+        
+        if(usernameLet == '' ||
+            emailLet == '' ||
+            senhaLet == '' ||
+            confirmarSenha == ''
+        ){
+            document.getElementById('validar_vazios').style.display = 'flex';
+            document.getElementById('span_vazios').style.display = 'block';
+
+            setInterval(function(){
+                document.getElementById('validar_vazios').style.display = 'none';
+                document.getElementById('span_vazios').style.display = 'none';
+            }, 5000);
+
+            return false;
+        }
+
+        fetch('/usuarios/cadastrar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                usernameServer: usernameLet,
+                emailServer: emailLet,
+                senhaServer: senhaLet
+            }),
+        })
+        .then(function(resposta){
+            console.log('Resposta: ', resposta);
+
+            if(resposta.ok){
+                document.getElementById('span_registro_realizado').style.display = 'block';
+
+                setInterval(alterLoginRegis(), 2000);
+            } else{
+                document.getElementById('span_registro_realizado').style.display = 'block';
+                span_registro_realizado.innerHTML = 'Falha no registro';
+            }
+        })
+
+    }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -53,10 +53,9 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nome = req.body.usernameServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +64,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
